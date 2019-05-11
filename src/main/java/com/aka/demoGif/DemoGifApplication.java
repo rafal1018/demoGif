ecf49fe97892d58c9d99a1765e0d7b85433beff7
rafal <rafaljaneczek@o2.pl>
2019-05-11 13:59:55 +0200

    commit

---
+++ src/main/java/com/aka/demoGif/DemoGifApplication.java
@@ -0,0 +1,13 @@
+package com.aka.demoGif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGifApplication.class, args);
+	}
+
+}
